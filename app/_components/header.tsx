e86aeae78b6bcc04a92b1a8ce64d779528044380
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2026-06-11 17:23:34 -0700

    fix link color highlighting

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -115,10 +115,10 @@ export function Header() {
 									}
 								>
 									<span
-										className={`nav-indicator mr-4 h-px bg-slate-600 transition-all motion-reduce:transition-none ${
+										className={`nav-indicator mr-4 h-px transition-all motion-reduce:transition-none ${
 											activeSection === item.id
 												? "w-16 bg-slate-200"
-												: "w-8 group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200"
+												: "w-8 bg-slate-600 group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200"
 										}`}
 									></span>
 									<span
